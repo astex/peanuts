db759eb49c2f2b73ac88435abe64bffd6689dbfd
astex <0astex@gmail.com>
2014-08-31 23:25:23 -0400

    database config

--- peanuts/__init__.py
+++ peanuts/__init__.py
@@ -15,4 +15,9 @@ def create_app(config):
     from peanuts.database import db
     db.init_app(app)
 
+    # This must be executed after all views have been registered so that all 
+    #   requirsite models have been imported.
+    with app.app_context():
+        db.create_all()
+
     return app
