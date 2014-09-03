9aaa0f0ac33397a9dbc75bd43ae01027bbe4f280
astex <0astex@gmail.com>
2014-09-02 23:47:21 -0400

    reply

--- peanuts/models/post.py
+++ peanuts/models/post.py
@@ -3,6 +3,8 @@
 
 from datetime import datetime
 
+from sqlalchemy.ext.declarative import declared_attr
+
 from peanuts.lib.database import db
 from peanuts.models.base import Model
 
@@ -27,3 +29,33 @@ class Post(Model):
         nullable=False
         )
     content = db.Column(db.UnicodeText, nullable=False)
+    state = db.Column(db.Enum('draft', 'posted', 'deleted'), default='draft')
+
+class PostSubModel(Model):
+    """An abstract base for Post submodels."""
+    __abstract__ = True
+
+    @declared_attr
+    def post(cls):
+        """The relationship to the post."""
+        return db.relationship('Post', backref='data')
+
+class Reply(Model):
+    """A more detailed view of a posting with additional information relevant
+        to replies.
+    """
+    __tablename__ = 'reply'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True)
+    parent_post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    post = db.relationship(
+        'Post',
+        backref='reply_data',
+        primaryjoin='Reply.post_id == Post.id_'
+        )
+    parent_post = db.relationship(
+        'Post',
+        backref='replies',
+        primaryjoin='Reply.parent_post_id == Post.id_'
+        )
