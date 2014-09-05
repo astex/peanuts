98655854d0ceb18fc8cdca1d323c88c3a5a2bf33
astex <0astex@gmail.com>
2014-09-05 09:30:03 -0400

    first admin test; second admin test;

--- peanuts/tests/test_user.py
+++ peanuts/tests/test_user.py
@@ -27,3 +27,42 @@ class UserTest(RestTestCase):
 
         data = r.json['data']
         assert 'user_id' in data
+
+    def test_post_first_admin(self):
+        """Tests that an admin may be posted to /user/ POST if there are no
+            other users."""
+        super(UserTest, self)._test_post({
+            'email': 'test@example.org',
+            'password': '123abc',
+            'confirm_password': '123abc',
+            'is_admin': True
+            }, verbosity='self')
+
+    def test_post_second_admin(self):
+        """Tests that an admin can post a second admin to /user/ POST and stay
+            logged in.
+        """
+        r = self.post(self.base_url + '/', data={
+            'email': 'test@example.org',
+            'password': '123abc',
+            'confirm_password': '123abc',
+            'is_admin': True
+            }, query_string={'verbosity': 'admin'})
+        assert 'data' in r.json
+        assert 'id' in r.json['data']
+        first_admin_id = r.json['data']['id']
+
+        # The first admin is logged in, create a second.
+        r = self.post(self.base_url + '/', data={
+            'email': 'test2@example.org',
+            'password': '123abc',
+            'confirm_password': '123abc',
+            'is_admin': True
+            })
+        assert r.status_code == 201
+
+        # Check that the first admin is still logged in.
+        r = self.get('/session/')
+        assert 'data' in r.json
+        assert 'user_id' in r.json['data']
+        assert first_admin_id == r.json['data']['user_id']
