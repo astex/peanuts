9409468874d13a4a72422aba884129678f7a646f
astex <0astex@gmail.com>
2014-09-07 14:21:15 -0400

    session has an application

--- peanuts/lib/session.py
+++ peanuts/lib/session.py
@@ -9,6 +9,25 @@ __all__ = ['PeanutsSessionInterface']
 
 class PeanutsSession(SecureCookieSession):
     """A custom session object for use with peanuts."""
+    def __init__(self, *args, **kargs):
+        """Initializes this with an application uuid taken from the request
+            headers.
+        """
+        super(PeanutsSession, self).__init__(*args, **kargs)
+
+        from flask import request
+        self.application_id = request.headers.get('X-PEANUTS-APPLICATION')
+
+    @property
+    def application(self):
+        """The application, taken from the database, if it exists."""
+        from peanuts.lib.database import db
+        from peanuts.models.app import Application
+        if self.application_id:
+            return db.session.query(Application).get(application_id)
+        else:
+            return None
+
     @property
     def user(self):
         """The user, taken from the database, if it exists."""
