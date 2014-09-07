a8c9c66d1d000f832b5d73cd3db14b744fcade7d
astex <0astex@gmail.com>
2014-09-07 16:18:04 -0400

    added ADMIN_REPO to test config

--- peanuts/config/test.py
+++ peanuts/config/test.py
@@ -2,3 +2,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite://'
 TESTING = True
 SERVER_NAME = '127.0.0.1:5993'
 SECRET_KEY = '\xe4\xc7\x99]\xb6`x\xd96\xb6\xaa\x11\xdbD\xc4X\xf5>\xc6\xa6=uDI'
+
+ADMIN_REPO = 'https://github.com/astex/peanuts.admin.git'
