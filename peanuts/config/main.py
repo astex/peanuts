68cf8834e52e977aa1d61b4e4d6926952e58d352
astex <0astex@gmail.com>
2014-09-07 14:27:48 -0400

    config.ADMIN_REPO

--- peanuts/config/main.py
+++ peanuts/config/main.py
@@ -2,3 +2,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///peanuts.db'
 DEBUG = True
 SERVER_NAME = '127.0.0.1:5992'
 SECRET_KEY = '\xb1,\x9c?\x1b#\x14\x83\x83`(6vk!t\xa3\x82\x17\xf9\x11dm\x7f'
+
+ADMIN_REPO = 'https://github.com/astex/peanuts.admin'
