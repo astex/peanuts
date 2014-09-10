631699a3dbd81b0f02e1d42cf94a2db84726819d
astex <0astex@gmail.com>
2014-09-10 10:35:17 -0400

    added admin token to conig

--- peanuts/__init__.py
+++ peanuts/__init__.py
@@ -41,7 +41,7 @@ def create_app(config):
                 # Unless overridden by a direct database operation, the first
                 #   app to be accessed should be admin.
                 admin_app = Application(
-                    token=str(uuid.uuid4()),
+                    token=app.config['ADMIN_TOKEN'],
                     title='Admin',
                     description='The administrative application.',
                     slug='admin',
--- peanuts/config/main.py
+++ peanuts/config/main.py
@@ -3,4 +3,5 @@ DEBUG = True
 SERVER_NAME = '127.0.0.1:5992'
 SECRET_KEY = '\xb1,\x9c?\x1b#\x14\x83\x83`(6vk!t\xa3\x82\x17\xf9\x11dm\x7f'
 
-ADMIN_REPO = 'https://github.com/astex/peanuts.admin'
+ADMIN_REPO = 'https://github.com/astex/peanuts.admin.git'
+ADMIN_TOKEN = '6752e4b0-8122-4c9a-ad1d-19d703fdfed0'
