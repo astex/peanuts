e403d305ad465b559f0684fcd21e84c38deeb546
astex <0astex@gmail.com>
2014-09-14 16:49:12 -0400

    error response is more complete

--- peanuts/lib/err.py
+++ peanuts/lib/err.py
@@ -10,8 +10,17 @@ __all__ = ['make_json_error']
 
 def make_json_error(exception):
     """Raises a jsonic error intead of the default Werkzeug HTML."""
-    response = jsonify(message=str(exception))
-    response.status_code = (
-        exception.code if isinstance(exception, HTTPException) else 500
-        )
+    from flask import request
+
+    response = jsonify({
+        'status': exception.code,
+        'method': request.method,
+        'url': request.url,
+        'message': (
+            exception.description if
+            hasattr(exception, 'description') else
+            str(exception)
+            )
+        }), exception.code if isinstance(exception, HTTPException) else 500
+
     return response
