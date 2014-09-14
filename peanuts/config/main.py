6cca05a95ae719ee238a3f699beab7293952989d
astex <0astex@gmail.com>
2014-09-14 00:08:56 -0400

    configuration changes

--- peanuts/config/main.py
+++ peanuts/config/main.py
@@ -3,4 +3,7 @@ DEBUG = True
 SERVER_NAME = '127.0.0.1:5992'
 SECRET_KEY = '\xb1,\x9c?\x1b#\x14\x83\x83`(6vk!t\xa3\x82\x17\xf9\x11dm\x7f'
 
+SESSION_COOKIE_NAME = 'peanuts'
+SESSION_COOKIE_DOMAIN = '127.0.0.1'
+
 ADMIN_REPO = 'https://github.com/astex/peanuts.admin.git'
--- peanuts/config/test.py
+++ peanuts/config/test.py
@@ -3,4 +3,6 @@ TESTING = True
 SERVER_NAME = '127.0.0.1:5993'
 SECRET_KEY = '\xe4\xc7\x99]\xb6`x\xd96\xb6\xaa\x11\xdbD\xc4X\xf5>\xc6\xa6=uDI'
 
+SESSION_COOKIE_NAME = 'peanuts_test'
+
 ADMIN_REPO = 'https://github.com/astex/peanuts.admin.git'
